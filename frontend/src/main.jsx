692ff2dd00bd6cf1738d82ce02c3537f7500477d
shivamNetke <netkeshiv3521@gmail.com>
2026-02-04 08:39:59 +0530

    040226

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React, { StrictMode } from 'react'   // ← React आणि StrictMode दोन्ही import
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+// import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
